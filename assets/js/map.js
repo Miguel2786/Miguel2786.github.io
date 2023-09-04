f106c04f6304a1e8aecbb695431b3c22dd000702
miguel <campos2787@gmail.com>
2023-09-04 09:42:36 +0200

    Agregar archivos iniciales

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",(event) => {
+
+    setTimeout(()=>{
+        document.querySelector("#load-iframe-map").innerHTML=`<iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.2351002563573!2d-5.976011224599393!3d37.384272034559196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126ea328d922c1%3A0x669e9d19c4624ee8!2sC.%20Luis%20de%20Morales%2C%2024%2C%2041018%20Sevilla!5e0!3m2!1ses!2ses!4v1693737628225!5m2!1ses!2ses"></iframe>
+
+    `;
+    },500);
+    
+});
